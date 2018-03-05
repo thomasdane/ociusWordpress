053608b56f7ad47d6a5648949acaba7c84decb46
Kevinwochan <kevinwochan@gmail.com>
2018-03-06 07:52:12 +1100

    Adding Google Analytics to header

--- header.php
+++ header.php
@@ -38,7 +38,17 @@
 	<style> .wf-loading h1 { font-family: "droid-sans"; visibility: hidden; } .wf-active h1 { visibility: visible; } </style>
 
 	<?php wp_head(); ?>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111430663-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
 
+	  gtag('config', 'UA-111430663-1');
+	</script>
+	
 </head>
 
 <body <?php body_class(); ?>>
